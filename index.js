32d78d60d4e5bd7a01fae2725558a735c235e8f0
Ivan Sanchez Gutmann <ivanchezg@gmail.com>
2018-10-20 22:32:57 -0500

    Add more functionalities

--- index.js
+++ index.js
@@ -1,77 +1,23 @@
 const minesTotal = 10;
 const mines = [];
 const buttons = [];
-let mainDiv;
+const rows = 10;
+const cols = 10;
+let buttonsDiv;
+
+// Configure with input text number of cols, rows and total mines: Check how it is done by microsoft and google
+// Move the mouse event check to a separate function
+// Change win and game over alert for text on top of div
+// Check browser compatibility
 
 window.onload = () => {
     createMinesweeper();
-}
-
-window.oncontextmenu = function ()
-{
-    // showCustomMenu();
-    return false;     // cancel default menu
 }
 
 function createMinesweeper() {
     createMines();
     createMainDiv();
     createButtons();
-}
-
-function createMainDiv() {
-    // Create div that will contain all buttons
-    mainDiv = document.createElement('div');
-    mainDiv.style.width = '160px';
-    mainDiv.style.height = '160px';
-    mainDiv.style.background = 'grey';
-    mainDiv.style.color = 'white';
-
-    document.getElementById('minesweeper').appendChild(mainDiv);
-}
-
-function createButtons() {
-    // Create all buttons
-    for (let row = 0; row < 10; row++) {
-        const rowButtons = [];
-        for (let col = 0; col < 10; col++) {
-            const button = document.createElement('button');
-            button.style.width = '16px';
-            button.style.height = '16px';
-            button.style.display = 'inline';
-            button.style.textAlign = 'center';
-            button.style.color = 'black';
-            button.onmouseup = (event) => {
-                console.dir(event);
-
-                let isRightClick;
-                if ("which" in event) { // Gecko (Firefox), WebKit (Safari/Chrome) & Opera
-                    isRightClick = event.which === 3; 
-                }
-                else if ("button" in event) { // IE, Opera 
-                    isRightClick = event.button === 2;
-                }                
-
-                console.log("Right mouse button " + (isRightClick ? "" : "was not ") + "clicked!");
-
-                if (isAMine(row, col)) {
-                    // Game Over
-                    alert('BOOM');
-                    revealMines();
-                }
-
-                if (isRightClick) {
-                    button.innerText = 'F';
-                } else {
-                    button.disabled = true;
-                }
-            }
-            
-            mainDiv.appendChild(button);
-            rowButtons.push(button);
-        }
-        buttons.push(rowButtons);
-    }
 }
 
 function createMines() {
@@ -86,9 +32,113 @@ function createMines() {
             count += 1;
         }
     }
+}
 
-    // Delete after testing
-    mines.forEach(mine => console.log(mine.row + "," + mine.col));
+function createMainDiv() {
+    // Create div that will contain all buttons
+    buttonsDiv = document.createElement('div');
+    buttonsDiv.oncontextmenu = () => {
+        return false;
+    }
+    buttonsDiv.style.width = '160px';
+    buttonsDiv.style.height = '160px';
+    buttonsDiv.style.background = 'grey';
+    buttonsDiv.style.color = 'white';
+
+    document.getElementById('minesweeper').appendChild(buttonsDiv);
+}
+
+function createButtons() {
+    // Create all buttons
+    for (let row = 0; row < rows; row++) {
+        const rowButtons = [];
+        for (let col = 0; col < cols; col++) {
+            const button = document.createElement('button');
+            button.style.width = '16px';
+            button.style.height = '16px';
+            button.style.display = 'inline';
+            button.style.textAlign = 'center';
+            button.style.color = 'black';
+            button.style.fontSize = '12px';
+            button.style.padding = '0px';
+            button.onmouseup = (event) => {
+                let isRightClick;
+                if ("which" in event) { // Gecko (Firefox), WebKit (Safari/Chrome) & Opera
+                    isRightClick = event.which === 3; 
+                } else if ("button" in event) { // IE, Opera 
+                    isRightClick = event.button === 2;
+                }
+
+                if (isRightClick) {
+                    if (button.innerText === 'F') {
+                        button.innerText = '';
+                    } else {
+                        button.innerText = 'F';
+                    }
+                } else {
+                    if (button.innerText === 'F') return;
+
+                    if (isAMine(row, col)) {
+                        alert('BOOM');
+                        revealMines();
+                        disableAllButtons();
+                    } else {
+                        revealSquare(row, col);
+                        checkWin();
+                    }
+                }
+            }
+            
+            buttonsDiv.appendChild(button);
+            rowButtons.push(button);
+        }
+        buttons.push(rowButtons);
+    }
+}
+
+function revealSquare(row, col) {
+    if (row < 0 || col < 0) return;
+    if (row >= rows || col >= cols) return;    
+    if (buttons[row][col].disabled) return;        
+    if (isAMine(row, col)) return;
+
+    const minesAround = countMinesAround(row, col);
+
+    if (minesAround > 0) {
+        buttons[row][col].innerText = minesAround;
+        buttons[row][col].disabled = true;
+        return;
+    }
+
+    buttons[row][col].disabled = true;
+    buttons[row][col].style.backgroundColor = 'grey';
+
+    revealSquare(row - 1, col - 1);
+    revealSquare(row - 1, col);
+    revealSquare(row - 1, col + 1);
+    revealSquare(row, col - 1);
+    revealSquare(row, col + 1);
+    revealSquare(row + 1, col - 1);
+    revealSquare(row + 1, col);
+    revealSquare(row + 1, col + 1);
+}
+
+function countMinesAround(row, col) {
+    let count = 0;
+    for (let i = row - 1; i <= row + 1; i++) {
+        for (let j = col - 1; j <= col + 1; j++) {
+            if (i < 0 || i > rows) {
+                break;
+            }
+            if (j < 0 || j > cols) {
+                continue;
+            }
+            if (isAMine(i, j)) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
 
 function revealMines() {
@@ -101,4 +151,30 @@ function revealMines() {
 function isAMine(row, col) {
     const isMine = mines.find(mine => mine.row == row && mine.col == col);
     return isMine ? true : false;
+}
+
+function disableAllButtons() {
+    buttons.forEach(row => {
+        row.forEach(button => {
+            button.disabled = true;
+        })
+    })
+}
+
+function checkWin() {
+    let countDisabled = 0;
+    buttons.forEach(row => {
+        row.forEach(button => {
+            if (button.disabled) {
+                countDisabled++;
+            }
+        })
+    })
+
+    console.log(countDisabled);
+
+    if (countDisabled === (rows * cols - minesTotal)) {
+        disableAllButtons();
+        alert('Winner');
+    }
 }
